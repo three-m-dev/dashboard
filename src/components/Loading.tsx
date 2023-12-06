6d07d9188948674408042e6a42997cd7a4be27f8
Jacob Reppuhn <jreppuhn@three-m.com>
2023-12-06 07:16:24 -0500

    loading screen styling

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,8 @@
-import { Layout } from ".";
-
 const Loading = () => {
   return (
-    <Layout>
-      <div className="flex h-full w-full items-center justify-center">
-        <div className="h-32 w-32 animate-spin rounded-full border-b-4 border-t-4 border-blue-500"></div>
-      </div>
-    </Layout>
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
+      <div className="h-32 w-32 animate-spin rounded-full border-b-4 border-t-4 border-blue-500"></div>
+    </div>
   );
 };
 

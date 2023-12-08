58592e59236d1d505e2f7d14ef6a6290ac7926d3
jacobreppuhn <jakereppuhn@gmail.com>
2023-12-08 01:01:21 -0500

    gn

--- src/components/charts/Output.tsx
+++ src/components/charts/Output.tsx
@@ -4,34 +4,40 @@ const Output = () => {
       <div className="container mx-auto px-4">
         <div className="overflow-hidden rounded shadow">
           <div className="bg-indigo-500">
-            <h3 className="p-6 text-xl font-bold text-white">
-              Sales Monitoring
-            </h3>
+            <h3 className="p-6 text-xl font-bold text-white">Output</h3>
             <div className="chart" data-type="area" data-variant="dark-bg" />
           </div>
           <div className="-m-3 flex flex-wrap bg-white px-6 py-10">
             <div className="w-1/2 p-3">
               <div className="rounded border px-6 py-4">
-                <p className="text-xs text-gray-500">Sales per user</p>
-                <h3 className="text-3xl font-bold">$1035</h3>
+                <p className="text-xs text-gray-500">
+                  This week's expected output
+                </p>
+                <h3 className="text-3xl font-bold">$245,000</h3>
               </div>
             </div>
             <div className="w-1/2 p-3">
               <div className="rounded border px-6 py-4">
-                <p className="text-xs text-gray-500">Inventory</p>
-                <h3 className="text-3xl font-bold">$435,122</h3>
+                <p className="text-xs text-gray-500">
+                  This week's actual output
+                </p>
+                <h3 className="text-3xl font-bold">$190,00</h3>
               </div>
             </div>
             <div className="w-1/2 p-3">
               <div className="rounded border px-6 py-4">
-                <p className="text-xs text-gray-500">Value of refunds</p>
-                <h3 className="text-3xl font-bold">$53,525</h3>
+                <p className="text-xs text-gray-500">
+                  Last week's expected output
+                </p>
+                <h3 className="text-3xl font-bold">$220,000</h3>
               </div>
             </div>
             <div className="w-1/2 p-3">
               <div className="rounded border px-6 py-4">
-                <p className="text-xs text-gray-500">Revenue</p>
-                <h3 className="text-3xl font-bold">$2,090,00</h3>
+                <p className="text-xs text-gray-500">
+                  Last week's actual output
+                </p>
+                <h3 className="text-3xl font-bold">$205,000</h3>
               </div>
             </div>
           </div>

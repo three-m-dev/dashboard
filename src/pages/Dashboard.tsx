aae22c2ce5259ce4e4baf0b65649ff4bade2262b
Jacob Reppuhn <jreppuhn@three-m.com>
2023-12-11 09:14:47 -0500

    dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,54 @@
 import { useState } from "react";
 import { Layout } from "../components";
+import PageHeader from "../components/reusable/PageHeader";
 
 const Dashboard = () => {
-  const [viewMode, setViewMode] = useState("overview");
+  const [activeTab, setActiveTab] = useState("overview");
 
-  const handleViewModeChange = (view: string) => {
-    if (viewMode !== view) {
-      setViewMode(view);
+  const renderContent = () => {
+    switch (activeTab) {
+      case "overview":
+        return <div>Overview</div>;
+      case "calendar":
+        return <div>Contacts</div>;
+      case "improvements":
+        return <div>Improvements</div>;
+      default:
+        return <div>Overview</div>;
     }
   };
 
-  return <Layout></Layout>;
+  const handleClick = () => {
+    console.log("Button Clicked");
+  };
+
+  const tabs = [
+    {
+      value: "overview",
+      buttons: [{ label: "fullscreen", onClick: handleClick }],
+    },
+    {
+      value: "calendar",
+      buttons: [{ label: "fullscreen", onClick: handleClick }],
+    },
+    {
+      value: "improvements",
+      buttons: [{ label: "fullscreen", onClick: handleClick }],
+    },
+  ];
+
+  return (
+    <Layout>
+      <PageHeader
+        title="Dashboard"
+        tabs={tabs}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+
+      {renderContent()}
+    </Layout>
+  );
 };
 
 export default Dashboard;

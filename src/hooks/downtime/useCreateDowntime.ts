e9a4fc60b5dfc77680e7de1cd4c8a8c73954ede3
Jacob Reppuhn <jreppuhn@three-m.com>
2023-12-20 11:01:50 -0500

    added create downtime hook

---
+++ src/hooks/downtime/useCreateDowntime.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import axios from "axios";
+import { baseUrl } from "../../utils/config";
+import { IDowntimeEntry } from "../../shared/interfaces";
+
+export const useCreateDowntimeEntry = () => {
+  const [downtimeEntry, setDowntimeEntry] = useState<IDowntimeEntry | null>(
+    null,
+  );
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const createDowntimeEntry = async (downtimeEntryData: any) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.post(
+        `${baseUrl}/downtime`,
+        downtimeEntryData,
+        {
+          withCredentials: true,
+        },
+      );
+      setDowntimeEntry(response.data);
+    } catch (error: any) {
+      setError(error.response ? error.response.data.message : error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { createDowntimeEntry, downtimeEntry, loading, error };
+};

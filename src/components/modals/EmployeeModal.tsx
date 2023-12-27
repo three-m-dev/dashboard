bf4a95f0f6e9e73ccaaa6a7a11e36ac202c8ce70
Jacob Reppuhn <jreppuhn@three-m.com>
2023-12-27 08:04:23 -0500

    started employee modal

--- src/components/modals/EmployeeModal.tsx
+++ src/components/modals/EmployeeModal.tsx
@@ -1,4 +1,5 @@
 import { IEmployee } from "../../shared/interfaces";
+import { formatDate, formatKebab } from "../../utils/formatter";
 import ModalBase from "../reusable/ModalBase";
 
 type Props = {
@@ -12,12 +13,49 @@ const EmployeeModal = ({ mode, onClose, selectedEmployee }: Props) => {
     <ModalBase
       title={
         mode === "view" && selectedEmployee
-          ? selectedEmployee.firstName + " " + selectedEmployee.lastName
+          ? "Team Member Details"
           : "Add Team Member"
       }
       onClose={onClose}
     >
-      <></>
+      {mode === "view" && selectedEmployee ? (
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-3 flex flex-col">
+            <label className="font-semibold">First Name</label>
+            <p>{selectedEmployee.firstName}</p>
+          </div>
+          <div className="col-span-3 flex flex-col">
+            <label className="font-semibold">Last Name</label>
+            <p>{selectedEmployee.lastName}</p>
+          </div>
+          <div className="col-span-6 flex flex-col">
+            <label className="font-semibold">Email</label>
+            <p>{selectedEmployee.email}</p>
+          </div>
+          <div className="col-span-3 flex flex-col">
+            <label className="font-semibold">Company</label>
+            <p>{formatKebab(selectedEmployee.company)}</p>
+          </div>
+          <div className="col-span-3 flex flex-col">
+            <label className="font-semibold">Department</label>
+            <p>{selectedEmployee.departmentId}</p>
+          </div>
+          <div className="col-span-3 flex flex-col">
+            <label className="font-semibold">Type</label>
+            <p>{formatKebab(selectedEmployee.type)}</p>
+          </div>
+          <div className="col-span-3 flex flex-col">
+            <label className="font-semibold">Start Date</label>
+            <p>{formatDate(selectedEmployee.startDate)}</p>
+          </div>
+          <div className="col-span-3 flex flex-col">
+            <label className="font-semibold">Status</label>
+            <p>{formatKebab(selectedEmployee.status)}</p>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </ModalBase>
   );
 };
